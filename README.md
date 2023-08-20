bf35872abaa61098dce625fd2effc61d7780c5c4
aap9002 <aap9002@gmail.com>
2023-08-20 21:40:51 +0100

    year 1 and 2 units

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Unit-Map
-
+[![.github/workflows/deploy.yml](https://github.com/AAP9002/Unit-Map/actions/workflows/deploy.yml/badge.svg)](https://github.com/AAP9002/Unit-Map/actions/workflows/deploy.yml)
 ## Description
 [Cytoscape Js](https://js.cytoscape.org/#demos)
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,53 +1,82 @@
 import cytoscape from "cytoscape";
 import "./map.css"
+import { useEffect } from "react";
+import CourseData from "../templates/UOMCS.json"
 
 function Map() {
-    var cy = cytoscape({
-        container: document.getElementsByClassName('graph')[0],
-        elements: [ // list of graph elements to start with
-            { // node a
-                data: { id: 'a' }
-            },
-            { // node b
-                data: { id: 'b' }
-            },
-            { // edge ab
-                data: { id: 'ab', source: 'a', target: 'b' }
-            }
-        ],
+    // const [userData, setUserData] = useState([]);
 
-        style: [ // the stylesheet for the graph
-            {
-                selector: 'node',
-                style: {
-                    'background-color': '#666',
-                    'label': 'data(id)'
+    useEffect(() => {
+        var data = CourseData;
+        console.log(data);
+        //setUserData(data);
+
+        var cy = cytoscape({
+            container: document.getElementsByClassName('graph')[0],
+            style: [
+                {
+                    selector: 'node',
+                    style: {
+                        'background-color': '#666',
+                        'label': 'data(name)'
+                    }
+                },
+    
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 3,
+                        'line-color': '#ccc',
+                        'target-arrow-color': '#ccc',
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier'
+                    }
                 }
-            },
-
-            {
-                selector: 'edge',
-                style: {
-                    'width': 3,
-                    'line-color': '#ccc',
-                    'target-arrow-color': '#ccc',
-                    'target-arrow-shape': 'triangle',
-                    'curve-style': 'bezier'
-                }
+            ],
+    
+            layout: {
+                name: 'grid',
+                rows: 1
             }
-        ],
+        },[]);
 
-        layout: {
-            name: 'grid',
-            rows: 1
+        const populateMap = (data) => {
+            var units = data.Units
+            var row = [0,0,0,0,0,0]
+            units.forEach(element => {
+                if(row[element.Year]>=0)
+                {
+                    row[element.Year] += 1
+                    row[element.Year] = row[element.Year] * -1
+                }
+                else
+                {
+                    row[element.Year] -= 1
+                    row[element.Year] = row[element.Year] * -1
+                }
+
+                cy.add({
+                    group: 'nodes',
+                    data: { id:element.Code, name: element.Code + " - "+element.Name, weight: element.Credits },
+                    position: { x:700*element.Year, y: 60*row[element.Year] }
+                });
+            });
+            units.forEach(element => {
+                var edges = element.preReqs;
+                var name = element.Code;
+                edges.forEach(edge => {
+                    cy.add({
+                        group: 'edges',
+                        data: { source: name, target: edge }
+                    });
+                });
+            });
+
+            cy.layout({ name: 'preset'}).run();
+            cy.fit();
         }
-    });
-
-    cy.add({
-        group: 'nodes',
-        data: { weight: 75 },
-        position: { x: 10, y: 10 }
-    });
+        populateMap(data);
+    }, []);
 
     return (<div className="graph"></div>);
 }
