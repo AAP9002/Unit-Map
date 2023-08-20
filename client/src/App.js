f76a9c2a8031d0aa290d935517c97fcb698e0f5d
aap9002 <aap9002@gmail.com>
2023-08-20 21:46:27 +0100

    rename

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Map from './components/Map.js';
 function App() {
   return (
     <div className="App">
-      <h1>Unit Map</h1>
+      <h1 style={{zIndex:100, width:"100vw", textAlign:"center"}}>UoM BSc Computer Science</h1>
       <Map/>
     </div>
   );
